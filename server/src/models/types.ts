4211b92a2dbbccf54d744a47db939634ed650eec
Tomáš Sabol <tomassabol2@gmail.com>
2024-09-30 11:17:07 +0200

    Add node types

--- server/src/models/types.ts
+++ server/src/models/types.ts
@@ -1,6 +1,6 @@
 import { type InferSchemaType } from "mongoose";
-import { type destinationSchema } from "./destination-model";
-import { type userSchema } from "./user-model";
+import { type destinationSchema } from "./destination-model.js";
+import { type userSchema } from "./user-model.js";
 
 export type Destination = InferSchemaType<typeof destinationSchema>;
 export type User = InferSchemaType<typeof userSchema>;
